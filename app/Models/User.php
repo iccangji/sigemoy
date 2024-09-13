d875be07e09945d57c9d52714c3d8c15b21bb447
iccangji <iksanpml131@gmail.com>
2024-09-14 01:54:59 +0800

    tambah fitur login backend

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'user',
+        'id',
     ];
 
     /**
@@ -39,7 +38,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('auth');
+Route::get('/login', [UserController::class, 'index'])->name('login');
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
+Route::get('/user', [UserController::class, 'show']);
