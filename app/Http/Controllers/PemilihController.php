fa17b2d6324100c8c92ccc5141380029801df263
iccangji <iksanpml131@gmail.com>
2024-09-16 01:19:14 +0800

    add pemilih backend

--- app/Http/Controllers/PemilihController.php
+++ app/Http/Controllers/PemilihController.php
@@ -2,19 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pemilih;
 use Illuminate\Http\Request;
 
 class PemilihController extends Controller
 {
     public function index()
     {
+        $dataPemilih = Pemilih::limit(1)->get();
+        // foreach ($dataPemilih as $item) {
+        //     echo $item;
+        // }
         return view(
             'pages.pemilih',
             [
                 'page' => 'pemlih',
                 'user' => auth()->user()->user,
-                'level' => auth()->user()->level
+                'level' => auth()->user()->level,
+                'data' => $dataPemilih
             ]
         );
+    }
+
+    public function getPemilihData(Request $request)
+    {
+        $size = $request->input('size');
+        $offset = $request->input('offset');
+        if ($size && $offset >= 0) {
+            $dataPemilih = Pemilih::limit($size)->offset($offset)->get();
+            return $dataPemilih;
+        }   
+        $data =  Pemilih::get();
+        return response()->json($data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,12 @@ Route::post('/logout', [UserController::class, 'logout']);
 Route::get('/', [IndexController::class, 'index'])->middleware(['auth', 'cors']);
 Route::get('/pemilih', [PemilihController::class, 'index'])->middleware(['auth', 'cors']);
 
-Route::get('/data-index', [IndexController::class, 'indexData'])->middleware('auth');
+//
+Route::get('/pemilih-data', [PemilihController::class, 'getPemilihData']);
+//
+
+
+Route::get('/data-index', [IndexController::class, 'indexData']);
 Route::get('/details/{nama_kecamatan}', [IndexController::class, 'detailsKecamatan'])->middleware('auth');
 Route::get('/details/{nama_kecamatan}/{nama_kelurahan}', [IndexController::class, 'detailsKelurahan'])->middleware('auth');
 Route::get('/Admin-Dashboard', [ViewController::class, 'DashboardAdmin']);
