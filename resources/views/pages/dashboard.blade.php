9bd306749cf517f6d1bd64e948bb409570209784
iccangji <iksanpml131@gmail.com>
2024-09-15 21:57:48 +0800

    minor change sidebar

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -38,7 +38,7 @@
                     </div>
                 </div>
             </div>
-            @if ($user!='penginput')
+            @if ($level!='penginput')
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                 <div class="card card-statistic-1">
                     <div class="card-icon bg-warning">
