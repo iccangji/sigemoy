a9bf74ddfa87b3499d5d6c14b62ce0dde84f1d42
iccangji <iksanpml131@gmail.com>
2024-10-28 01:42:44 +0800

    fix export data pemilih

--- app/Exports/PemilihExport.php
+++ app/Exports/PemilihExport.php
@@ -8,24 +8,29 @@ use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class PemilihExport implements FromCollection, WithHeadings
+class PemilihExport implements WithColumnFormatting, FromCollection, WithHeadings
 {
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-        return Pemilih::all([
-            'nama',
-            'nik',
-            'no_hp',
-            'hub_keluarga',
-            'tps',
-            'kelurahan',
-            'kecamatan',
-            'nama_pj',
-            'no_hp_pj'
-        ]);
+        return Pemilih::all(
+            [
+                'nama',
+                'nik',
+                'no_hp',
+                'hub_keluarga',
+                'tps',
+                'kelurahan',
+                'kecamatan',
+                'nama_pj',
+                'no_hp_pj'
+            ]
+        )->map(function ($item) {
+            $item->nik = "'$item->nik";
+            return $item;
+        });;
     }
 
     public function headings(): array
@@ -43,10 +48,11 @@ class PemilihExport implements FromCollection, WithHeadings
             'No HP Penanggung Jawab'
         ];
     }
-    // public function columnFormats(): array
-    // {
-    //     return [
-    //         'B' => NumberFormat::FORMAT_NUMBER, // Mengatur kolom 'C' (Phone Number) sebagai angka
-    //     ];
-    // }
+    public function columnFormats(): array
+    {
+        return [
+            'B' => NumberFormat::FORMAT_TEXT, // Mengatur kolom 'C' (Phone Number) sebagai angka
+            'C' => NumberFormat::FORMAT_TEXT, // Mengatur kolom 'C' (Phone Number) sebagai angka
+        ];
+    }
 }
